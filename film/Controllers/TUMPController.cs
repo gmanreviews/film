f9dd87e3a56ed6ab584514f2da8f6d10d24d45f6
gmanreviews <andrew.robinson@gmanreviews.com>
2016-05-03 23:13:25 -0500

    working on TUMP league functions

--- film/Controllers/TUMPController.cs
+++ film/Controllers/TUMPController.cs
@@ -13,5 +13,11 @@ namespace film.Controllers
         {
             return View();
         }
+
+        public ActionResult League(int id = 0)
+        {
+            if (id == 0) return RedirectToAction("Index");
+            else return View();
+        }
     }
 }
--- film/Models/league.cs
+++ film/Models/league.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
 
 namespace film.Models
 {
@@ -10,5 +11,14 @@ namespace film.Models
         public int id { get; set; }
         public string league_name { get; set; }
         public List<team> teams { get; set; }
+    }
+    public class league_model
+    {
+        public static league get_league(int id)
+        {
+            league league = new league();
+
+            return league;
+        }
     }
 }
